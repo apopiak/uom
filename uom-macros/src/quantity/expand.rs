e89cda7a1f43e394e20443cfe0ab95c191ee3cd3
Mike Boutin <mike.boutin@gmail.com>
2019-05-13 09:29:42 -0400

    [ci skip] Expand

--- uom-macros/src/quantity/expand.rs
+++ uom-macros/src/quantity/expand.rs
@@ -1,9 +1,34 @@
 use super::*;
 use proc_macro2::TokenStream;
+use quote::quote;
+// use syn::{AttrStyle, Token};
 
 /// Expand the `quantity!` macro invocation to define the parsed quantity and associated units.
 pub(crate) fn expand(input: Quantity) -> Result<TokenStream, syn::Error> {
-    drop(input);
+    let Quantity {
+        name_attributes,
+        name,
+        // description,
+        // dimension_attributes,
+        // system,
+        // dimensions,
+        // kind,
+        // units,
+        ..
+    } = input;
+    // let mod_comments = name_attributes
+    //     .iter()
+    //     .map(|a| {
+    //         Attribute {
+    //             style: AttrStyle::Inner(Token![!]([a.pound_token.span.clone()])),
+    //             ..a.clone()
+    //         }
+    //     });
 
-    Ok(TokenStream::new())
+    let ts = quote! {
+        #(#name_attributes)*
+        pub struct #name;
+    };
+
+    Ok(ts)
 }
--- uom-macros/src/system/expand.rs
+++ uom-macros/src/system/expand.rs
@@ -1,10 +1,44 @@
 use super::*;
 use proc_macro2::TokenStream;
+use quote::quote;
+// use syn::{AttrStyle, Token};
 
 /// Expand the `system!` macro invocation to define the system of quantities and default system of
 /// units.
 pub(crate) fn expand(input: System) -> Result<TokenStream, syn::Error> {
-    drop(input);
+    let System {
+        name_attributes,
+        name,
+        // units_attributes,
+        // units,
+        // base_quantities,
+        quantities,
+        ..
+    } = input;
+    // let mod_comments = name_attributes
+    //     .iter()
+    //     .map(|a| {
+    //         Attribute {
+    //             style: AttrStyle::Inner(Token![!]([a.pound_token.span.clone()])),
+    //             ..a.clone()
+    //         }
+    //     });
+    let mods = quantities.iter().filter_map(|q| {
+        if q.add_mod {
+            Some(quote! { mod #(q.module); })
+        } else {
+            None
+        }
+    });
 
-    Ok(TokenStream::new())
+    let ts = quote! {
+        // #(#mod_comments)*
+
+        #(#name_attributes)*
+        pub struct #name;
+
+        #(#mods)*
+    };
+
+    Ok(ts)
 }
